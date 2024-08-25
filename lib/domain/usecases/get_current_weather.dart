7f2733e5c758f01b0f754b124e88b2cde13fe70d
flutterWithChris <94571307+flutterWithChris@users.noreply.github.com>
2024-08-25 13:38:34 -0400

    Use Case Testing, Mock Weather Repo, Failure Class

---
+++ lib/domain/usecases/get_current_weather.dart
@@ -0,0 +1,17 @@
+import 'package:dartz/dartz.dart';
+
+import 'package:weather_clean_architecture_tdd/core/error/failure.dart';
+import 'package:weather_clean_architecture_tdd/domain/entities/weather.dart';
+import 'package:weather_clean_architecture_tdd/domain/repositories/weather_repository.dart';
+
+class GetCurrentWeatherUseCase {
+  final WeatherRepository weatherRepository;
+
+  GetCurrentWeatherUseCase({
+    required this.weatherRepository,
+  });
+
+  Future<Either<Failure, WeatherEntity>> execute(String cityName) async {
+    return weatherRepository.getCurrentWeather(cityName);
+  }
+}
---
+++ test/domain/usecases/get_current_weather_test.dart
@@ -0,0 +1,42 @@
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:weather_clean_architecture_tdd/domain/entities/weather.dart';
+import 'package:weather_clean_architecture_tdd/domain/usecases/get_current_weather.dart';
+
+import '../../helpers/test_helper.mocks.dart';
+
+void main() {
+  late MockWeatherRepository mockWeatherRepository;
+  late GetCurrentWeatherUseCase getCurrentWeatherUseCase;
+
+  setUp(() {
+    mockWeatherRepository = MockWeatherRepository();
+    getCurrentWeatherUseCase =
+        GetCurrentWeatherUseCase(weatherRepository: mockWeatherRepository);
+  });
+
+  const testWeatherDetail = WeatherEntity(
+    cityName: 'New York',
+    main: 'Clouds',
+    description: 'few clouds',
+    iconCode: '02d',
+    temperature: 302.28,
+    humidity: 70,
+    pressure: 1009,
+  );
+
+  const testCityName = 'New York';
+
+  test('Should get current weather detail from repo.', () async {
+    // arrange
+    when(mockWeatherRepository.getCurrentWeather(testCityName))
+        .thenAnswer((_) async => const Right(testWeatherDetail));
+
+    // act
+    final result = await getCurrentWeatherUseCase.execute(testCityName);
+
+    // assert
+    expect(result, const Right(testWeatherDetail));
+  });
+}
